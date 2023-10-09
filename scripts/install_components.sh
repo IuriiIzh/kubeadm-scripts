4040c816483905be91d863929c575df98a39bba5
Iurii Izhevskii <iurii.izhevskii@jamalschiro.com>
2023-10-09 11:41:27 +0200

    added metric server

--- scripts/install_components.sh
+++ scripts/install_components.sh
@@ -7,5 +7,8 @@ set -euo pipefail
 # Install calico networking plugin.
 #kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 
+# Install metrik server
+kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
+
 
 
