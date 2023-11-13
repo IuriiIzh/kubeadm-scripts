83746b74a9c384ab0d7a6f34d6bfdaf74f2d767f
Iurii Izhevskii <iurii.izhevskii@jamalschiro.com>
2023-11-13 22:05:11 +0100

    added argocd to components

--- scripts/install_components.sh
+++ scripts/install_components.sh
@@ -13,4 +13,33 @@ kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/m
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-
+cat <<EOF | kubectl apply -f -
+kind: Service
+apiVersion: v1
+metadata:
+  name: argocd-server-node
+  namespace: argocd
+  labels:
+    app.kubernetes.io/component: server
+    app.kubernetes.io/name: argocd-server
+    app.kubernetes.io/part-of: argocd
+spec:
+  ports:
+    - name: http
+      protocol: TCP
+      port: 80
+      targetPort: 8080
+    - name: https
+      protocol: TCP
+      port: 443
+      targetPort: 8080
+      nodePort: 32444
+  selector:
+    app.kubernetes.io/name: argocd-server
+  type: NodePort
+  sessionAffinity: None
+  ipFamilies:
+    - IPv4
+  ipFamilyPolicy: SingleStack
+  internalTrafficPolicy: Cluster
+EOF
