65c0d1fcca57df109a93f9157063b3689019407a
Iurii Izhevskii <iurii.izhevskii@jamalschiro.com>
2023-11-16 10:43:22 +0100

    updated reset

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -4,13 +4,19 @@
 
 set -eo pipefail
 
-sudo kubeadm reset
+sudo kubeadm reset -f
 
 sudo rm -rf /etc/kubernetes/
+sudo rm -rf /etc/cni/
+sudo rm -rf /etc/crio/
+sudo rm -rf /etc/containers/
 
 sudo rm -rf /opt/kubernetes/
-sudo rm -rf /etc/cni/
 sudo rm -rf /opt/cni/
+
+sudo crictl ps -q | xargs -n 1 crictl stop
+sudo crictl ps -q | xargs -n 1 crictl rm
+
 sudo firewall-cmd --permanent --delete-zone 000-kubernetes
 
 sudo systemctl reboot
